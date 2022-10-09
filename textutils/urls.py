0bc526437cec9a072a1068128f76c8cb97f9b75e
shubhamborate <83327985+shubhamborate@users.noreply.github.com>
2022-10-09 10:24:37 +0530

    Update urls.py

--- textutils/urls.py
+++ textutils/urls.py
@@ -20,14 +20,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('analyze', views.analyze, name='analyze'),
     path('about_us/', views.about_us,name='about_us'),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    
 ]
